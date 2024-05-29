8779b64bb8937db75b8b19eb07f7d667f9586567
cyprich <cypooriginal@gmail.com>
2024-05-29 22:46:14 +0200

    last
    last commit (hopefully) before starting new tutorial 'c++ for java developers' or something like that...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    int a;
+    int b;
+
+    std::cout << "Enter the value of a and b: ";
+    std::cin >> a >> b;
+
+    std::cout << "Area of rectangle = " << a << " * " << b << " = " << a * b;
+
     return 0;
 }
